cb58763c24f22e4f483afba5e3a57ce965f1ce0e
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-05-07 21:39:20 +0530

    Add redirectRouter.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('express-async-errors');
 const newRouter = require('./routes/newRouter');
+const redirectRouter = require('./routes/redirectRouter');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
 app.use('/new', newRouter);
+app.use('/', redirectRouter);
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`listening on http://localhost:${9000}`));
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
-  if (error) res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).send(error.details[0].message);
 
   const { lastCode } = (
     await db.collection('details').doc('info').get()
