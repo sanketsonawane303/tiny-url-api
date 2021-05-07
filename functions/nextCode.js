4e4e71eb6d115eda839bf6c3d3fd7862dc64b5ae
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-05-07 19:53:37 +0530

    Add function to generate next code

---
+++ functions/nextCode.js
@@ -0,0 +1,19 @@
+const aplhabets = '0123456789abcdefghijklmnopqrstuvwxyz';
+const radix = aplhabets.length;
+
+module.exports = function (lastCode) {
+  let numVal = 0;
+  lastCode.split('').forEach((ch) => {
+    numVal = numVal * radix + aplhabets.indexOf(ch);
+  });
+
+  numVal++;
+
+  let nextCode = '';
+  while (numVal > 0) {
+    nextCode = aplhabets[numVal % radix] + nextCode;
+    numVal = parseInt(Math.floor(numVal / radix));
+  }
+
+  return nextCode;
+};
