487defbff892a62adf18411e8d4167055aafa83f
sanketsonawane303 <62289047+sanketsonawane303@users.noreply.github.com>
2021-05-07 21:16:19 +0530

    Add newRouter.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const db = require('./firebase/config');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('express-async-errors');
+const newRouter = require('./routes/newRouter');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+app.use('/new', newRouter);
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`listening on http://localhost:${9000}`));
---
+++ routes/newRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const Joi = require('joi');
+const db = require('../firebase/config');
+const nextCode = require('../functions/nextCode');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+
+  const { lastCode } = (
+    await db.collection('details').doc('info').get()
+  ).data();
+
+  const code = nextCode(lastCode);
+
+  const data = {
+    code,
+    url: req.body.url,
+    count: 0,
+  };
+
+  await db.collection('urls').doc(code).set(data);
+  await db.collection('details').doc('info').set({ lastCode: code });
+
+  res.status(200).send({ code });
+});
+
+function validate(url) {
+  const schema = Joi.object({
+    url: Joi.string().required(),
+  });
+
+  return schema.validate(url);
+}
+
+module.exports = router;
